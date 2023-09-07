bb6cb9f9dd904aede98e0258236d05a506a609b2
Rebecca Robison <rebeccahrobison@outlook.com>
2023-09-07 10:25:44 -0500

    Changed transient state object to map data structure

--- scripts/TransientState.js
+++ scripts/TransientState.js
@@ -1,19 +1,24 @@
 // Set up the transient state data structure and provide initial valuess
-const transientState = {
-        "ownsBlueJeans": false,
-        "socioLocationId": 0
-}
+// const transientState = {
+//         "ownsBlueJeans": false,
+//         "socioLocationId": 0
+// }
 
+const transientState = new Map()
+transientState.set("ownsBlueJeans", false)
+transientState.set("socioLocationId", 0)
 
 
 // Functions to modify each property of transient state
 export const setOwnsBlueJeans = (chosenOwnership) => {
-    transientState.ownsBlueJeans = chosenOwnership
+    // transientState.ownsBlueJeans = chosenOwnership
+    transientState.set("ownsBlueJeans", chosenOwnership)
     console.log(transientState)
 } 
 
 export const setSocioLocationId = (chosenLocation) => {
-    transientState.socioLocationId = chosenLocation
+    // transientState.socioLocationId = chosenLocation
+    transientState.set("socioLocationId", chosenLocation)
     console.log(transientState)
 } 
 
@@ -21,12 +26,13 @@ export const setSocioLocationId = (chosenLocation) => {
 
 // Function to convert transient state to permanent state
 export const saveSurveySubmission = async () => {
+    const objectifiedTransientState = Object.fromEntries(transientState)
     const postOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(transientState)
+        body: JSON.stringify(objectifiedTransientState)
     }
 
     const response = await (fetch("http://localhost:8088/submissions", postOptions))
